c43d46c7ac58962ed7d465ccebc7ea00f219cfbc
aminbasiran <aminbasiran@aminbasirans-MacBook-Air.local>
2024-09-16 08:19:39 +0800

    help

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>please god hopefully this works please bro</h3>
-        <h3>please god hopefully this works please bro</h3>
-        <h3>please god hopefully this works please bro</h3>
-        <h3>please god hopefully this works please bro</h3>
+        <h3>please god hopefully this works please bro sis</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
